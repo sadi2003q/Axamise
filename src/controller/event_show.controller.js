7fe52d6052bf9ef6abd14cb313992dc5f70adcac
Md Adnan abdullah Sadi <113715329+sadi2003q@users.noreply.github.com>
2025-09-22 03:22:20 +0000

    [Added][working]
    Event Show service function is now done

---
+++ src/controller/event_show.controller.js
@@ -0,0 +1,27 @@
+import { tablePaginationClasses } from '@mui/material'
+import { EventShowService } from '../services/_event_show.service.js'
+
+
+
+export class EventShowController {
+    constructor() {
+        this.service = EventShowService()
+    }
+
+    
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
