d058ee0e50dda2da5eebacec937f087f6440c5e0
sadi <adnanabdullah625@gmail.com>
2025-09-28 14:39:37 +0600

    [Modified]
    Admin Set User is now complete

---
+++ src/Components/_Admin_SetUser.jsx
@@ -0,0 +1,237 @@
+/* eslint-disable react-refresh/only-export-components */
+
+
+import { Background_Particles } from "../Components/__Admin_Login";
+
+import MenuItem from "@mui/material/MenuItem";
+import { Admin_Info } from "../models/AdminInfo_Model";
+import { GetCommonProps2 } from "../Components/__Common.jsx";
+import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
+import { Admin_InfoController } from "../controller/admin.setUser.controller.js";
+import SearchIcon from "@mui/icons-material/Search";
+import IconButton from '@mui/material/IconButton';
+import AdminPanelSettingsTwoToneIcon from '@mui/icons-material/AdminPanelSettingsTwoTone';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+ // Styles object
+export const style = {
+    outerDiv: "w-screen h-screen flex justify-center items-center z-20",
+    formContainer: "w-[40vw] h-[84vh] bg-gray-400 m-4 rounded-2xl flex flex-col justify-between p-8 gap-6 overflow-auto",
+    hideScrollbar: {
+        scrollbarWidth: "none"
+    },
+    innerFlex: "w-full flex justify-between",
+    leftFields: "m-1 flex flex-col gap-4",
+    adminListContainer: "w-[30vw] h-[84vh] bg-gray-400 m-4 rounded-2xl p-8 flex flex-col justify-between z-500",
+    listHeading: "text-2xl font-semibold mb-4 text-gray-800",
+};
+
+export const Heading = ({headingText, handleAddNewAdmin}) => {
+    return(
+        <div className="w-full h-auto m-2 flex items-center justify-between">
+            <div className="flex items-center space-x-2">
+                <h1 className="text-2xl font-roboto font-bold text-gray-300">
+                    {headingText}
+                </h1>
+            </div>
+            <IconButton aria-label="Add new Amdin" color="inherit" className="text-white"
+                        onClick={handleAddNewAdmin}>
+                <AdminPanelSettingsTwoToneIcon />
+            </IconButton>
+        </div>
+    );
+}
+
+export const NameField = ({adminInfo, handleChange}) => {
+    return(
+        <TextField
+            {...GetCommonProps2({ labelColor: "4a5568", focusColor: "#bcedb4", textColor: "white", borderColor: "black" })}
+            label="Name"
+            name="name"
+            value={adminInfo.name}
+            onChange={handleChange}
+            required
+        />
+    );
+}
+
+export const EmailField = ({adminInfo, handleChange, buttonText}) => {
+    return (
+        <TextField
+            {...GetCommonProps2({ labelColor: "4a5568", focusColor: "#bcedb4", textColor: "white", borderColor: "black" })}
+            label="Email"
+            name="email"
+            value={adminInfo.email}
+            onChange={handleChange}
+            required
+            slotProps={{
+                input: {
+                    readOnly: buttonText === "Update"
+                }
+            }}
+        />
+    );
+}
+
+export const ProfilePictureField = ({adminInfo, handleImageChange}) => {
+    return(
+        <div className="m-1 z-2000">
+            <label className="cursor-pointer">
+                <input
+                    type="file"
+                    accept="image/*"
+                    className="hidden"
+                    onChange={handleImageChange}
+                />
+                <div className="w-[180px] h-[128%] bg-gray-700 rounded-lg border border-black flex items-center justify-center overflow-hidden">
+                    {adminInfo.profilePicture ? (
+                        <img
+                            src={adminInfo.profilePicture}
+                            alt="Profile Preview"
+                            className="w-full h-full object-cover"
+                        />
+                    ) : (
+                        <span className="text-white text-sm">Click to Upload</span>
+                    )}
+                </div>
+            </label>
+        </div>
+    );
+}
+
+export const RoleField = ({adminInfo, handleChange}) => {
+    return(
+        <TextField
+            {...GetCommonProps2({ labelColor: "4a5568", focusColor: "#bcedb4", textColor: "white", borderColor: "black" })}
+            select
+            label="Role"
+            name="role"
+            value={adminInfo.role}
+            onChange={handleChange}
+            required
+        >
+            <MenuItem value="Approval Department">Approval Department</MenuItem>
+            <MenuItem value="User Management Department">User Management Department</MenuItem>
+        </TextField>
+    );
+}
+
+export const PasswordField = ({adminInfo, handleChange, buttonText}) => {
+    return(
+
+            <TextField
+                {...GetCommonProps2({ labelColor: "4a5568", focusColor: "#bcedb4", textColor: "white", borderColor: "black" })}
+                label="Password"
+                name="password"
+                value={adminInfo.password}
+                onChange={handleChange}
+                required
+                slotProps={{
+                    input: {
+                        readOnly: buttonText === "Update"
+                    }
+                }}
+            />
+
+
+    );
+}
+
+
+export const PhoneNumberField = ({adminInfo, handleChange}) => {
+    return(
+
+    <TextField
+        {...GetCommonProps2({ labelColor: "4a5568", focusColor: "#bcedb4", textColor: "white", borderColor: "black" })}
+        label="Phone Number"
+        name="phoneNumber"
+        value={adminInfo.phoneNumber}
+        onChange={handleChange}
+        required
+    />
+
+    );
+}
+
+
+
+export const AddressField = ({adminInfo, handleChange}) => {
+    return (
+
+        <TextField
+            {...GetCommonProps2({ labelColor: "4a5568", focusColor: "#bcedb4", textColor: "white", borderColor: "black" })}
+            label="Address"
+            name="address"
+            value={adminInfo.address}
+            onChange={handleChange}
+            required
+        />
+    );
+}
+
+export const SubmitButton = ({handleSubmit, buttonText}) => {
+    return (
+        <Button variant="contained"
+                sx={{
+                    backgroundColor: '#4A5568',
+                    '&:hover': {
+                        backgroundColor: '#2D3748',
+                    },
+                    marginTop: '16px',
+                    alignSelf: 'flex-end',
+                    textTransform: 'none',
+                    fontSize: '16px',
+                    padding: '8px 24px',
+                }}
+                onClick={handleSubmit}
+        >
+            {buttonText}
+        </Button>
+    );
+}
+
+
+export const SearchItems = ({searchTerm, searchAdmins}) => {
+    return (
+        <div className="flex items-center mb-4 border-b border-gray-600">
+            <SearchIcon className="text-gray-600 mr-2" />
+            <input
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={searchAdmins}
+                className="w-full bg-transparent focus:outline-none text-gray-800 py-1"
+            />
+        </div>
+    );
+}
+
+
+export const AdminList = ({admins, filteredNames, handleListItemClick, handleDelete }) => {
+    return (
+        <div className="flex-1 overflow-auto">
+            {admins.length > 0 ? (
+                filteredNames.map((admin, index) => (
+                    <div
+                        key={index}
+                        className="py-2 px-2 mb-1 rounded hover:bg-gray-300 cursor-pointer text-gray-800 flex justify-between"
+                        onClick={() => handleListItemClick(admin)}
+                    >
+                        {admin.name}
+                        <div>
+                            <IconButton
+                                onClick={() => handleDelete(admin.id)}
+                            >
+                                <DeleteIcon />
+                            </IconButton>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <div className="text-gray-500">No results found</div>
+            )}
+        </div>
+    );
+}
--- src/pages/Admin_SetUser.jsx
+++ src/pages/Admin_SetUser.jsx
@@ -1,33 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-unused-vars */
 import { Background_Particles } from "../Components/__Admin_Login";
 import { useState, useEffect } from "react";
-import MenuItem from "@mui/material/MenuItem";
-import { Admin_Info } from "../models/AdminInfo_Model";
-import { GetCommonProps2 } from "../Components/__Common.jsx";
-import TextField from "@mui/material/TextField";
-import Button from '@mui/material/Button';
-import { Admin_InfoController } from "../controller/admin.setUser.controller.js";
-import SearchIcon from "@mui/icons-material/Search";
-import IconButton from '@mui/material/IconButton';
-import AdminPanelSettingsTwoToneIcon from '@mui/icons-material/AdminPanelSettingsTwoTone';
-import DeleteIcon from '@mui/icons-material/Delete';
 
-/**
- * LEFT: CHANGE EMAIL AND PASSWORD
- * @returns {JSX.Element}
- * @constructor
- */
+import { Admin_Info } from "../models/AdminInfo_Model";
+import { Admin_InfoController } from "../controller/admin.setUser.controller.js";
+
+import { Heading, NameField, EmailField, ProfilePictureField, RoleField, PasswordField, PhoneNumberField, AddressField, SubmitButton, SearchItems, AdminList, style } from "../Components/_Admin_SetUser.jsx";
+
+
+
+
 
 export default function Admin_SetUsr() {
 
+
+    // Variables and States
     const [admins, setAdmins] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
-
-
     const [buttonText, setButtonText] = useState("Add");
     const [headingText, setHeadingText] = useState("Add New Admin");
-
     const [adminID, setAdminID] = useState("");
 
 
@@ -35,6 +26,7 @@ export default function Admin_SetUsr() {
         admin.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
+    // Admin Information State
     const [adminInfo, setAdminInfo] = useState(
         new Admin_Info({
             name: "Admin Name",
@@ -51,9 +43,13 @@ export default function Admin_SetUsr() {
         })
     );
 
-
     const controller = new Admin_InfoController(adminInfo, setAdmins, adminID);
 
+    /**
+     * Handle input changes for admin information fields
+     * @param {*} e - The event object
+     * @returns {void}
+     */
     const handleChange = (e) => {
         setAdminInfo(prev => ({
             ...prev,
@@ -61,6 +57,10 @@ export default function Admin_SetUsr() {
         }));
     };
 
+    /**
+     * Handle the addition of a new admin
+     * @returns {void}
+     */
     const handleAddNewAdmin = () => {
         setAdminInfo(new Admin_Info({
             name: "",
@@ -76,6 +76,11 @@ export default function Admin_SetUsr() {
         setHeadingText("Add New Admin");
     }
 
+    /**
+     * Handle the selection of an admin from the list   
+     * @param {*} admin 
+     * @returns {void}
+     */
     const handleListItemClick = (admin) => {
         setAdminInfo(new Admin_Info({
             name: admin.name || "",
@@ -94,6 +99,15 @@ export default function Admin_SetUsr() {
         setHeadingText("Update Admin");
         setAdminID(admin.id);
     };
+
+
+
+    /**
+     * 
+     * @param {*} e 
+     * @returns {void}
+     * Handles the change of profile picture by reading the selected file and updating the adminInfo state with the new image data.
+     */
     const handleImageChange = (e) => {
         const file = e.target.files[0];
         if (file) {
@@ -107,6 +121,14 @@ export default function Admin_SetUsr() {
             reader.readAsDataURL(file);
         }
     };
+
+
+    /**
+     * 
+     * @param {*} id 
+     * @returns {void}
+     * Handles the deletion of an admin by calling the controller's deleteAdmin method and updating the local admins state to remove the deleted admin from the UI.
+     */
     const handleDelete = (id) => {
         controller.deleteAdmin(id).then(() => {
             console.log("Admin Deleted Successfully from UI file");
@@ -118,13 +140,9 @@ export default function Admin_SetUsr() {
 
 
     /**
-     * Handle form submission
-     * @param e
-     * @returns {Promise<void>}
-     * @description This function handles the form submission based on the button text.
-     * If the button text is "Add", it calls the handleEmailSignUp method from the controller.
-     * If the button text is "Update", it calls the updateAdmin method from the controller.
-     * It also logs the result of the operation.
+     * Handles the form submission for adding or updating an admin
+     * @param {*} e - The event object
+     * @returns {void}
      */
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -138,7 +156,7 @@ export default function Admin_SetUsr() {
 
                 // Append it to the existing admins array
                 setAdmins(prevAdmins => [...prevAdmins, adminInfo]);
-
+                // console.log(adminInfo)
                 break;
             case "Update":
                 controller.updateAdmin().then(() => {
@@ -152,214 +170,67 @@ export default function Admin_SetUsr() {
                 }).catch((error) => {
                     console.log("Error updating admin:", error);
                 });
+
+                // console.log(adminInfo)
                 break;
         }
-
     };
 
+    /**
+     * 
+     * @param {*} e 
+     * @returns {void}
+     * Handles the search input change to filter admins by name.
+     */
+    const searchAdmins = (e) => {
+        setSearchTerm(e.target.value)
+    }
+
     useEffect(() => {
-        controller.getAllAdmins()
+        controller.getAllAdmins().then(() => {
+            console.log("Admins fetched successfully using controller file");
+        })
     }, []);
 
 
-
-
-
-
+    /**
+     * Renders the Admin_SetUser component with form fields and admin list
+     * @returns {JSX.Element}
+     */
     return (
         <>
             <Background_Particles />
 
-            <div className="w-screen h-screen flex justify-center items-center z-20">
+            <div className={style.outerDiv}>
                 <div
-                    className="w-[40vw] h-[84vh] bg-gray-400 m-4 rounded-2xl flex flex-col justify-between p-8 gap-6 overflow-auto"
-                    style={{
-                        scrollbarWidth: "none", // For Firefox
-                    }}
+                    className={style.formContainer}
+                    style={style.hideScrollbar}
                 >
-                    {/* Hide scrollbar for Webkit browsers (Chrome, Edge, Safari) */}
                     <style>
-                        {`
-      div::-webkit-scrollbar {
-        display: none;
-      }
-    `}
+                        {`div::-webkit-scrollbar { display: none; }`}
                     </style>
 
-                    {/*  Heading */}
-                    <div className="w-full h-auto m-2 flex items-center justify-between">
-                        <div className="flex items-center space-x-2">
-                            <h1 className="text-2xl font-roboto font-bold text-gray-300">
-                                {headingText}
-                            </h1>
+                    <Heading headingText={headingText} handleAddNewAdmin={handleAddNewAdmin} />
+
+                    <div className={style.innerFlex}>
+                        <div className={style.leftFields}>
+                            <NameField adminInfo={adminInfo} handleChange={handleChange} />
+                            <EmailField adminInfo={adminInfo} handleChange={handleChange} buttonText={buttonText} />
                         </div>
-                        <IconButton aria-label="Add new Amdin" color="inherit" className="text-white"
-                        onClick={handleAddNewAdmin}>
-                            <AdminPanelSettingsTwoToneIcon />
-                        </IconButton>
+                        <ProfilePictureField adminInfo={adminInfo} handleImageChange={handleImageChange} />
                     </div>
 
-
-                    {/*  Profile Image and Name*/}
-                    <div className="w-full flex justify-between">
-                        <div className="m-1 flex flex-col gap-4">
-                            {/* Name */}
-                            <TextField
-                                {...GetCommonProps2({ labelColor: "4a5568", focusColor: "#bcedb4", textColor: "white", borderColor: "black" })}
-                                label="Name"
-                                name="name"
-                                value={adminInfo.name}
-                                onChange={handleChange}
-                                required
-                            />
-
-                            {/* Email */}
-                            <TextField
-                                {...GetCommonProps2({ labelColor: "4a5568", focusColor: "#bcedb4", textColor: "white", borderColor: "black" })}
-                                label="Email"
-                                name="email"
-                                value={adminInfo.email}
-                                onChange={handleChange}
-                                required
-                                slotProps={{
-                                    input: {
-                                        readOnly: buttonText === "Update"
-                                    }
-                                }}
-                            />
-                        </div>
-
-                        {/* Profile Picture */}
-                        <div className="m-1 z-2000">
-                            <label className="cursor-pointer">
-                                <input
-                                    type="file"
-                                    accept="image/*"
-                                    className="hidden"
-                                    onChange={handleImageChange}
-                                />
-                                <div className="w-[180px] h-[128%] bg-gray-700 rounded-lg border border-black flex items-center justify-center overflow-hidden">
-                                    {adminInfo.profilePicture ? (
-                                        <img
-                                            src={adminInfo.profilePicture}
-                                            alt="Profile Preview"
-                                            className="w-full h-full object-cover"
-                                        />
-                                    ) : (
-                                        <span className="text-white text-sm">Click to Upload</span>
-                                    )}
-                                </div>
-                            </label>
-                        </div>
-                    </div>
-
-                    {/* Role */}
-                    <TextField
-                        {...GetCommonProps2({ labelColor: "4a5568", focusColor: "#bcedb4", textColor: "white", borderColor: "black" })}
-                        select
-                        label="Role"
-                        name="role"
-                        value={adminInfo.role}
-                        onChange={handleChange}
-                        required
-                    >
-                        <MenuItem value="Approval Department">Approval Department</MenuItem>
-                        <MenuItem value="User Management Department">User Management Department</MenuItem>
-                    </TextField>
-
-                    {/* Password */}
-                    <TextField
-                        {...GetCommonProps2({ labelColor: "4a5568", focusColor: "#bcedb4", textColor: "white", borderColor: "black" })}
-                        label="Password"
-                        name="password"
-                        value={adminInfo.password}
-                        onChange={handleChange}
-                        required
-                        slotProps={{
-                            input: {
-                                readOnly: buttonText === "Update"
-                            }
-                        }}
-                    />
-
-                    {/* Phone Number */}
-                    <TextField
-                        {...GetCommonProps2({ labelColor: "4a5568", focusColor: "#bcedb4", textColor: "white", borderColor: "black" })}
-                        label="Phone Number"
-                        name="phoneNumber"
-                        value={adminInfo.phoneNumber}
-                        onChange={handleChange}
-                        required
-                    />
-
-                    {/* Address */}
-                    <TextField
-                        {...GetCommonProps2({ labelColor: "4a5568", focusColor: "#bcedb4", textColor: "white", borderColor: "black" })}
-                        label="Address"
-                        name="address"
-                        value={adminInfo.address}
-                        onChange={handleChange}
-                        required
-                    />
-
-
-                    <Button variant="contained"
-                        sx={{
-                            backgroundColor: '#4A5568',
-                            '&:hover': {
-                                backgroundColor: '#2D3748',
-                            },
-                            marginTop: '16px',
-                            alignSelf: 'flex-end',
-                            textTransform: 'none',
-                            fontSize: '16px',
-                            padding: '8px 24px',
-                        }}
-                        onClick={handleSubmit}
-                    >
-                        {buttonText}
-                    </Button>
+                    <RoleField adminInfo={adminInfo} handleChange={handleChange} />
+                    <PasswordField adminInfo={adminInfo} handleChange={handleChange} buttonText={buttonText} />
+                    <PhoneNumberField adminInfo={adminInfo} handleChange={handleChange} />
+                    <AddressField adminInfo={adminInfo} handleChange={handleChange} />
+                    <SubmitButton handleSubmit={handleSubmit} buttonText={buttonText} />
                 </div>
 
-                <div className="w-[30vw] h-[84vh] bg-gray-400 m-4 rounded-2xl p-8 flex flex-col justify-between z-500">
-                    <h2 className="text-2xl font-semibold mb-4 text-gray-800" style={{ fontFamily: 'Times New Roman' }}>All Admins</h2>
-
-
-                    {/* Search bar */}
-                    <div className="flex items-center mb-4 border-b border-gray-600">
-                        <SearchIcon className="text-gray-600 mr-2" />
-                        <input
-                            type="text"
-                            placeholder="Search..."
-                            value={searchTerm}
-                            onChange={(e) => setSearchTerm(e.target.value)}
-                            className="w-full bg-transparent focus:outline-none text-gray-800 py-1"
-                        />
-                    </div>
-
-                    {/* Scrollable list */}
-                    <div className="flex-1 overflow-auto">
-                        {admins.length > 0 ? (
-                            filteredNames.map((admin, index) => (
-                                <div
-                                    key={index}
-                                    className="py-2 px-2 mb-1 rounded hover:bg-gray-300 cursor-pointer text-gray-800 flex justify-between"
-                                    onClick={() => handleListItemClick(admin)}
-                                >
-                                    {admin.name}
-                                    <div>
-                                        <IconButton
-                                            onClick={() => handleDelete(admin.id)}
-                                        >
-                                            <DeleteIcon />
-                                        </IconButton>
-                                    </div>
-                                </div>
-                            ))
-                        ) : (
-                            <div className="text-gray-500">No results found</div>
-                        )}
-                    </div>
+                <div className={style.adminListContainer}>
+                    <h2 className={style.listHeading} style={{ fontFamily: 'Times New Roman' }}>All Admins</h2>
+                    <SearchItems searchTerm={searchTerm} searchAdmins={searchAdmins} />
+                    <AdminList admins={admins} filteredNames={filteredNames} handleListItemClick={handleListItemClick} handleDelete={handleDelete} />
                 </div>
             </div>
         </>
